59df2672fd45a407c8a1e9d810dfc8a3f923abed
Aluno <davi.ishizuka@escola.pr.gov.br>
2025-10-02 07:53:27 -0300

    ATIVIDADE

---
+++ 2a.js
@@ -0,0 +1,5 @@
+let contador =0
+while(contador<5){ 
+    contador++
+    console.log(contador)
+}
---
+++ 2c.js
@@ -0,0 +1,7 @@
+let x=1
+x++
+let y=3
+let resultado=x*y
+while(resultado<=30){
+    console.log(resultado)
+}
